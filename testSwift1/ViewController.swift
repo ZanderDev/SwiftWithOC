a1e61aaf454f92b067217a154308f3549dac8e84
minghui zheng <minghuizheng@minghuideMacBook-Pro.local>
2017-09-14 08:22:56 +0800

    Initial Commit

---
+++ testSwift1/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  testSwift1
+//
+//  Created by minghui zheng on 2017/9/14.
+//  Copyright © 2017年 minghui zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var optional2: String? = .None
+        
+        print(optional2)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
